4949980e05e1c81c762dd3d65c8ea5eaca0dbed3
Goudacraft <goudacraft@gmail.com>
2013-10-26 12:46:49 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,36 @@
+package fr.elie.nombre;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+public class Main {
+	public static void main(String[] args){
+
+		try{
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		}catch (Exception e){
+			System.out.println("Unable to load Windows look and feel");
+		}
+		
+		
+		Frame GUI = new Frame();
+			
+		GUI.setSize(275, 100);
+		GUI.setLocationRelativeTo(null);
+		GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GUI.setVisible(true);
+		GUI.setResizable(false);
+		
+	
+			
+			
+		
+	
+		    
+		} 
+	
+	}
+	
+		
+	
+		
